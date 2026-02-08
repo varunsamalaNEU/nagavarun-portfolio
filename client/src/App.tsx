734b40ad4b3eb23ff35dcb3427ddf202fff1a6c1
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 <samala.n@northeastern.edu>
2026-02-07 21:17:53 -0500

    Add base path to Wouter router for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-
-function Router() {
+function Routes() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
@@ -24,7 +23,9 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router base="/nagavarun-portfolio">
+            <Routes />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
